8b793db1f55be44620968439685d860566e51d57
Scott Ertmer <scottertmer@gmail.com>
2021-10-26 17:21:51 -0600

    Add Error route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,26 @@ import Nav from '../Nav/Nav';
 import Projects from '../Projects/Projects';
 import Stills from '../Stills/Stills';
 import Contact from '../Contact/Contact';
-import { Route } from 'react-router-dom';
+import Error from '../Error/Error';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Nav />
-      <Route exact path='/projects' component={Projects} />
-      <Route exact path='/stills' component={Stills} />
-      <Route exact path='/contact' component={Contact} />
+      <Switch>
+        <Route exact path='/projects' component={Projects} />
+        <Route exact path='/stills' component={Stills} />
+        <Route exact path='/contact' component={Contact} />
+        <Route path='/' component={Projects} /> 
+        <Route path='*' component={Error} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// restructure HTML - <header> here in App, or in the component itself.
+// which is better for screen reader
