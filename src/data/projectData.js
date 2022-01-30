faba5e516167dd56bc5512ce8fdda993a34dc469
Scott Ertmer <scottertmer@gmail.com>
2022-01-30 11:16:03 -0700

    Fix stills URLs

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,48 +3,82 @@ const projects = [
     title: 'Southern Illinois University - Imagine',
     videoLink: 'https://youtu.be/KioJs5NrOyc',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/KioJs5NrOyc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1t2NluPKDesafrbPVCtrq0TjZwENRroUI/view?usp=sharing','https://drive.google.com/file/d/1Vwz0zkO4n37m5vtx6NuI-KltZ0u1-xm6/view?usp=sharing','https://drive.google.com/file/d/1WbqabbAjrK9cHPlUgEC1dcwoc9tBY_0R/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1t2NluPKDesafrbPVCtrq0TjZwENRroUI',
+      'https://drive.google.com/uc?export=view&id=1Vwz0zkO4n37m5vtx6NuI-KltZ0u1-xm6',
+      'https://drive.google.com/uc?export=view&id=1WbqabbAjrK9cHPlUgEC1dcwoc9tBY_0R'
+    ]
   },
   {
     title: 'Love Liberation - Music in the Moment',
     videoLink: 'https://youtu.be/fziNOkE8NcQ',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fziNOkE8NcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1BwQvIpLMGwUVR8jNsfGfxnq0W3nJsWD3/view?usp=sharing','https://drive.google.com/file/d/12SWUL5E-sfIYfcYqjJZT1-AnriQsNIoU/view?usp=sharing','https://drive.google.com/file/d/1A1Fn_F9291eFsj3aeww2jqk3f9P1t7bf/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1BwQvIpLMGwUVR8jNsfGfxnq0W3nJsWD3',
+      'https://drive.google.com/uc?export=view&id=12SWUL5E-sfIYfcYqjJZT1-AnriQsNIoU',
+      'https://drive.google.com/uc?export=view&id=1A1Fn_F9291eFsj3aeww2jqk3f9P1t7bf'
+    ]
   },
   {
     title: 'Colorado Camping - A Visual Diary',
     videoLink: 'https://youtu.be/vsKdQ-ucwYk',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/vsKdQ-ucwYk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1tZ7PvMWIQ56Ev2zLaceCJBRVOzehrW7O/view?usp=sharing','https://drive.google.com/file/d/1mDDRlWz1EPYff_1dQZSCoGG4xfqAXwG-/view?usp=sharing','https://drive.google.com/file/d/1mDDRlWz1EPYff_1dQZSCoGG4xfqAXwG-/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1tZ7PvMWIQ56Ev2zLaceCJBRVOzehrW7O',
+      'https://drive.google.com/uc?export=view&id=1mDDRlWz1EPYff_1dQZSCoGG4xfqAXwG-',
+      'https://drive.google.com/uc?export=view&id=1O4yC-dSIwSKR2VnGjTutZU1S3-O0ZJqU'
+    ]
   },
   {
     title: 'Fashion BTS',
     videoLink: 'https://youtu.be/TXFuySt2zEY',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/TXFuySt2zEY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1H58sFitnY_cAsWtfNfVwmETOhlqjRTi3/view?usp=sharing','https://drive.google.com/file/d/12vGjZ_DJRJxTHa4Q-HKvz_a3JeFaCIft/view?usp=sharing','https://drive.google.com/file/d/1rDMrdmtNOeZ2qlyS1rvOZ6RQywlPGar0/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1H58sFitnY_cAsWtfNfVwmETOhlqjRTi3',
+      'https://drive.google.com/uc?export=view&id=12vGjZ_DJRJxTHa4Q-HKvz_a3JeFaCIft',
+      'https://drive.google.com/uc?export=view&id=1rDMrdmtNOeZ2qlyS1rvOZ6RQywlPGar0'
+    ]
   },
   {
     title: 'Jackson Safety - All in a Day\'s Work',
     videoLink: 'https://youtu.be/pkIFH6RFvJc',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/pkIFH6RFvJc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1rQI7RNN5N1xANdt4x7ujZrqrA5fWVkkk/view?usp=sharing','https://drive.google.com/file/d/1sc18sjGRoj_vdDojaMqLW9H28U_L-Vuj/view?usp=sharing','https://drive.google.com/file/d/18piPZS837xhjutA_3svqAsnvPrjAtUBV/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1rQI7RNN5N1xANdt4x7ujZrqrA5fWVkkk',
+      'https://drive.google.com/uc?export=view&id=1sc18sjGRoj_vdDojaMqLW9H28U_L-Vuj',
+      'https://drive.google.com/uc?export=view&id=18piPZS837xhjutA_3svqAsnvPrjAtUBV'
+    ]
   },
   {
     title: 'Family Counseling Services - Hope Starts Here',
     videoLink: 'https://youtu.be/sNcPxtpvgeo',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/sNcPxtpvgeo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1KYgycD_u1uyUpeIWBJeXFSKCcSOM0aoL/view?usp=sharing','https://drive.google.com/file/d/1mrq2CfHq2ezKQwAr_dQb7SkZQXtr_B5m/view?usp=sharing','https://drive.google.com/file/d/1G2OHrKkE0mR4V9l81VTu-9A0AdERqLr5/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1KYgycD_u1uyUpeIWBJeXFSKCcSOM0aoL',
+      'https://drive.google.com/uc?export=view&id=1mrq2CfHq2ezKQwAr_dQb7SkZQXtr_B5m',
+      'https://drive.google.com/uc?export=view&id=1G2OHrKkE0mR4V9l81VTu-9A0AdERqLr5'
+    ]
   },
   {
     title: 'BC Merchants - Raise Resilient',
     videoLink: 'https://youtu.be/-3wWpooDr_I',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/-3wWpooDr_I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/17G1QH3H_LVi09PuozyJ9ODZkip9cRIHS/view?usp=sharing','https://drive.google.com/file/d/1J4tIgkJPaQBHXD0otEY_9sIVqIi9d-gH/view?usp=sharing','https://drive.google.com/file/d/1AIPFv8bYoaw727q9MnaK7bEe3zvjRyzj/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=17G1QH3H_LVi09PuozyJ9ODZkip9cRIHS',
+      'https://drive.google.com/uc?export=view&id=1J4tIgkJPaQBHXD0otEY_9sIVqIi9d-gH',
+      'https://drive.google.com/uc?export=view&id=1AIPFv8bYoaw727q9MnaK7bEe3zvjRyzj'
+    ]
   },
   {
     title: 'Southern Illinois University - Driven',
     videoLink: 'https://youtu.be/yGxZJefAqkA',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/yGxZJefAqkA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    stills: ['https://drive.google.com/file/d/1i2p5uUHWNrXLU6Fhig5X5qcnGBSqxBHi/view?usp=sharing','https://drive.google.com/file/d/1UZcTTeaj3nNj_ww5SB-sc7qBpuSs9rte/view?usp=sharing','https://drive.google.com/file/d/189HHPPHJJR-2Il6i7DFzjpPzSQWYmdXL/view?usp=sharing']
+    stills: [
+      'https://drive.google.com/uc?export=view&id=1i2p5uUHWNrXLU6Fhig5X5qcnGBSqxBHi',
+      'https://drive.google.com/uc?export=view&id=1UZcTTeaj3nNj_ww5SB-sc7qBpuSs9rte',
+      'https://drive.google.com/uc?export=view&id=189HHPPHJJR-2Il6i7DFzjpPzSQWYmdXL'
+    ]
   },
 ]
+
+module.exports = projects;
