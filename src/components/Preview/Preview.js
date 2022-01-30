81b70c6630dbc2cbf7ce725a8c0df4b1227e5cb4
Scott Ertmer <scottertmer@gmail.com>
2022-01-30 11:55:02 -0700

    Display stills

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,9 +1,17 @@
 import './Preview.css';
 
-export default function Preview() {
+
+export default function Preview({title, stills}) {
+  console.log(stills)
+  const images = stills.map(still => <img className='still' src={still} alt={`Still image from ${title} video`}/>)
   return(
-    <div class='preview-stills-container'>
-      preview
+    <div className='preview-stills-container'>
+      {images}
     </div>
   )
 }
+
+
+// Project component renders Preview component
+// each preview component displays three stills,
+// but the whole container is a link to the individual Project(?) comp
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,23 @@
 import './Projects.css';
-
+import Preview from '../Preview/Preview';
+import projects from '../../data/projectData';
 
 export default function Projects() {
+  const previews = projects.map(project => {
+    return (
+      <Preview title={project.title} stills={project.stills} />
+    )
+  })
 
   return (
-    <div>
+    <main>
       <p className='nav-tab'>PROJECTS</p>
-    </div>
+      {previews}
+    </main>
   )
 }
+
+// Project component renders Preview component
+  // pass stills, title for each project as props to Preview
+// each preview component displays three stills,
+// but the whole container is a link to the individual Project(?) comp
