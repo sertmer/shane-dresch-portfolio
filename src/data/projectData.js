d607aa127544ee742bde99c9024ab894914a7be5
Scott Ertmer <scottertmer@gmail.com>
2022-02-22 07:52:54 -0700

    Update projectData with aspect ratios

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -7,17 +7,19 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=1t2NluPKDesafrbPVCtrq0TjZwENRroUI',
       'https://drive.google.com/uc?export=view&id=1Vwz0zkO4n37m5vtx6NuI-KltZ0u1-xm6',
       'https://drive.google.com/uc?export=view&id=1WbqabbAjrK9cHPlUgEC1dcwoc9tBY_0R'
-    ]
+    ],
+    aspectRatio: '16:9'
   },
   {
     title: 'Love Liberation - Music in the Moment',
     videoLink: 'https://youtu.be/fziNOkE8NcQ',
     embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fziNOkE8NcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     stills: [
-      'https://drive.google.com/uc?export=view&id=1BwQvIpLMGwUVR8jNsfGfxnq0W3nJsWD3',
-      'https://drive.google.com/uc?export=view&id=12SWUL5E-sfIYfcYqjJZT1-AnriQsNIoU',
-      'https://drive.google.com/uc?export=view&id=1A1Fn_F9291eFsj3aeww2jqk3f9P1t7bf'
-    ]
+      'https://drive.google.com/uc?export=view&id=1ClIorkB1TQhniQ4nIa-sc3QDW3YOsh4P',
+      'https://drive.google.com/uc?export=view&id=1_wggIIXo-4HyhRavShZmbcoOhFj7TU0c',
+      'https://drive.google.com/uc?export=view&id=1Dt3WTO2hQn7wR-v8jY5nF8qd3fhXjbLN'
+    ],
+    aspectRatio: '16:9'
   },
   {
     title: 'Colorado Camping - A Visual Diary',
@@ -27,7 +29,8 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=1tZ7PvMWIQ56Ev2zLaceCJBRVOzehrW7O',
       'https://drive.google.com/uc?export=view&id=1mDDRlWz1EPYff_1dQZSCoGG4xfqAXwG-',
       'https://drive.google.com/uc?export=view&id=1O4yC-dSIwSKR2VnGjTutZU1S3-O0ZJqU'
-    ]
+    ],
+    aspectRatio: '2:35:1'
   },
   {
     title: 'Fashion BTS',
@@ -37,7 +40,8 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=1H58sFitnY_cAsWtfNfVwmETOhlqjRTi3',
       'https://drive.google.com/uc?export=view&id=12vGjZ_DJRJxTHa4Q-HKvz_a3JeFaCIft',
       'https://drive.google.com/uc?export=view&id=1rDMrdmtNOeZ2qlyS1rvOZ6RQywlPGar0'
-    ]
+    ],
+    aspectRatio: '2:35:1'
   },
   {
     title: 'Jackson Safety - All in a Day\'s Work',
@@ -47,7 +51,8 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=1rQI7RNN5N1xANdt4x7ujZrqrA5fWVkkk',
       'https://drive.google.com/uc?export=view&id=1sc18sjGRoj_vdDojaMqLW9H28U_L-Vuj',
       'https://drive.google.com/uc?export=view&id=18piPZS837xhjutA_3svqAsnvPrjAtUBV'
-    ]
+    ],
+    aspectRatio: '16:9'
   },
   {
     title: 'Family Counseling Services - Hope Starts Here',
@@ -57,7 +62,8 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=1KYgycD_u1uyUpeIWBJeXFSKCcSOM0aoL',
       'https://drive.google.com/uc?export=view&id=1mrq2CfHq2ezKQwAr_dQb7SkZQXtr_B5m',
       'https://drive.google.com/uc?export=view&id=1G2OHrKkE0mR4V9l81VTu-9A0AdERqLr5'
-    ]
+    ],
+    aspectRatio: '2:35:1'
   },
   {
     title: 'BC Merchants - Raise Resilient',
@@ -67,7 +73,8 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=17G1QH3H_LVi09PuozyJ9ODZkip9cRIHS',
       'https://drive.google.com/uc?export=view&id=1J4tIgkJPaQBHXD0otEY_9sIVqIi9d-gH',
       'https://drive.google.com/uc?export=view&id=1AIPFv8bYoaw727q9MnaK7bEe3zvjRyzj'
-    ]
+    ],
+    aspectRatio: '16:9'
   },
   {
     title: 'Southern Illinois University - Driven',
@@ -77,7 +84,8 @@ const projects = [
       'https://drive.google.com/uc?export=view&id=1i2p5uUHWNrXLU6Fhig5X5qcnGBSqxBHi',
       'https://drive.google.com/uc?export=view&id=1UZcTTeaj3nNj_ww5SB-sc7qBpuSs9rte',
       'https://drive.google.com/uc?export=view&id=189HHPPHJJR-2Il6i7DFzjpPzSQWYmdXL'
-    ]
+    ],
+    aspectRatio: '16:9'
   },
 ]
 
