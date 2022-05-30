113ce0c262f7a3cb4b7fca0fb4ce2aeb26cb08f0
Scott Ertmer <scottertmer@gmail.com>
2022-05-30 13:57:56 -0600

    Build initial contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,6 +2,12 @@ import './Contact.css';
 
 export default function Contact() {
   return (
-    <p className='nav-tab'>CONTACT</p>
+    <div>
+      <p>Shane Dresch</p>
+      <a href='mailto:shaneldresch@gmail.com'>shaneldresch@gmail.com</a>
+      <a href='https://www.instagram.com/anxiyeti/'>instagram</a>
+      <a href='https://www.youtube.com/user/zeroshane50'>youtube</a>
+      <a href='https://vimeo.com/user56392919'>vimeo</a>
+    </div>
   )
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 export default function Nav() {
   return(
     <nav>
-      <NavLink exact className='navigation-link' activeClassName='navigation-link-active' to='/projects'>Projects</NavLink>
+      <NavLink className='navigation-link' activeClassName='navigation-link-active' to='/projects'>Projects</NavLink>
       {/* <NavLink to='/stills'>Stills</NavLink> */}
-      <NavLink exact className='navigation-link' activeClassName='navigation-link-active' to='/contact'>Contact</NavLink>
+      <NavLink className='navigation-link' activeClassName='navigation-link-active' to='/contact'>Contact</NavLink>
     </nav>
   )
 }
