d7d0321c7967d9e6019a7bb412d3eb0b4f60ed49
Scott Ertmer <scottertmer@gmail.com>
2022-08-03 09:24:48 -0600

    Fix email address

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div>
       <p>Shane Dresch</p>
-      <a href='mailto:shaneldresch@gmail.com'>shaneldresch@gmail.com</a>
+      <a href='mailto:shane.l.dresch@gmail.com'>shane.l.dresch@gmail.com</a>
       <a href='https://www.instagram.com/anxiyeti/'>instagram</a>
       <a href='https://www.youtube.com/user/zeroshane50'>youtube</a>
       <a href='https://vimeo.com/user56392919'>vimeo</a>
